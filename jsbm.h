201d8aa669c7e00c502c411cc327f6ddbdfa27a1
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-06-01 20:08:02 -0400

    Primeiro commit

---
+++ jsbm.h
@@ -0,0 +1,15 @@
+/* Esta obra foi licenciada com a Licença Creative Commons
+ * Attribution-NonCommercial 3.0 Brazil. Para ver uma cópia desta licença,
+ * visite http://creativecommons.org/licenses/by-nc/3.0/br/ ou envie um 
+ * pedido por carta para Creative Commons, 444 Castro Street, Suite 900, 
+ * Mountain View, California, 94041, USA.
+ *
+ * Autor: Paulo Roberto Urio
+ *
+ * Biblioteca para simples medição de desempenho de trechos de códigos
+ * na linguagem C.
+ */
+#ifndef JSBM_INCLUDE_H
+#define JSBM_INCLUDE_H
+
+#endif
