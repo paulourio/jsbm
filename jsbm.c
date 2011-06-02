f72473c130f31a3c3ab8875125053668051b3a7f
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-06-01 21:58:45 -0400

    time.h sys/time.h

--- jsbm.c
+++ jsbm.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 #include <assert.h>
 #include <malloc.h>
 #include "jsbm.h"
--- jsbm.h
+++ jsbm.h
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/time.h>
 #include <assert.h>
 #include <malloc.h>
